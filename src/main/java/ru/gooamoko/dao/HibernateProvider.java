6db19bd4244a57c1e7787340d00a4f2f8f0f5c60
leonid <gooamokod@rambler.ru>
2014-08-28 21:37:33 +0700

    HibernateProvider was refactored

--- src/main/java/ru/gooamoko/dao/HibernateProvider.java
+++ src/main/java/ru/gooamoko/dao/HibernateProvider.java
@@ -1,21 +1,30 @@
 package ru.gooamoko.dao;
 
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 public class HibernateProvider {
-	private static SessionFactory factory;
-	private static ServiceRegistry registry;
 
-	public static SessionFactory getSessionFactory() {
-		Configuration configuration = new Configuration();
-		configuration.configure();
-		registry = new StandardServiceRegistryBuilder().applySettings(
-				configuration.getProperties()).build();
-		factory = configuration.buildSessionFactory(registry);
-		return factory;
-	}
+  private static final SessionFactory factory = buildFactory();
 
+  public static SessionFactory getSessionFactory() {
+    return factory;
+  }
+
+  private static SessionFactory buildFactory() {
+    try {
+      Configuration configuration = new Configuration();
+      configuration.configure();
+      ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(
+          configuration.getProperties()).build();
+      return configuration.buildSessionFactory(registry);
+    } catch (HibernateException ex) {
+      // Make sure you log the exception, as it might be swallowed
+      System.err.println("Initial SessionFactory creation failed." + ex);
+      throw new ExceptionInInitializerError(ex);
+    }
+  }
 }
